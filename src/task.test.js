f3a171d76ab2a1c0e627cdd1f05df7198cae59db
Bateyjosue <josuebatey19@gmail.com>
2022-08-19 17:39:57 +0200

    Add test for update status of the task to be completed or not

--- src/task.test.js
+++ src/task.test.js
@@ -1,4 +1,5 @@
 import Task from './task.js';
+import Status from './status.js';
 
 require('jest-localstorage-mock');
 
@@ -26,5 +27,21 @@ describe('Edit Task Testing', () => {
   });
   test('Should update the collection "1" to "Morning Prayer"', () => {
     expect(task.editTask(1, 'Morning Prayer')).toBe(task.tasks[1]);
+  });
+});
+
+describe('Update Status', () => {
+  const status = new Status();
+  status.task.tasks.push({ index: 0, description: 'Morning Session', completed: false });
+  status.task.tasks.push({ index: 1, description: 'Morning Prayer', completed: true });
+
+  it('Should return the updated Element', () => {
+    expect(status.updateStatus(0, true)).toBe(status.task.tasks[0].completed);
+  });
+  it('Should return the updated Element', () => {
+    expect(status.updateStatus(1, false)).toBe(status.task.tasks[1].completed);
+  });
+  it('Should return the updated Element', () => {
+    expect(status.updateStatus(1, true)).toBe(status.task.tasks[1].completed);
   });
 });
